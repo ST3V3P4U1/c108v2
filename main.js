b7f13dc18d184994cada18fbd397e4034b6c24aa
ST3V3P4U1 <110664964+ST3V3P4U1@users.noreply.github.com>
2023-09-08 17:39:21 +1000

    Add files via upload

---
+++ main.js
@@ -0,0 +1,31 @@
+function startclassification() {
+    navigator.mediaDevices.getUserMedia({audio:true});
+    classifier = ml5.soundClassifier('https://teachablemachine.withgoogle.com/models/P3UevWpnl/model.json',modelReady);
+}
+function modelReady() {
+    classifier.classify(gotResult);
+}
+dog=0;
+cat=0;
+function gotResult(error, results) {
+    if(error){
+        console.error(error);
+    }
+    else{
+        console.log(results);
+        document.getElementById("detected").innerHTML="Detected Dog -" +dog+"Detected Cat"+cat;
+        document.getElementById("voice").innerHTML="I Can Hear - "+results[0].label;
+        img=document.getElementById("animalimage");
+        if(results[0].label=="Bark"){
+            img.src="download(1).jpeg";
+            dog=dog+1;
+        }
+        else if(results[0].label=="Meow"){    
+        img.src="download.jpeg";
+        cat=cat+1;
+    }
+    else{
+        img.src="https://shravaripatil.github.io/Sound_controlled_animals/listen.gif";
+    }
+    }
+}
